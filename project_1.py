4e8c7b47c548e0152382ec10efd74da56adbe5d4
belchejs-UC <97567968+belchejs-UC@users.noreply.github.com>
2022-02-19 22:12:01 -0500

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -1,1 +1,39 @@
+operation = input('''
+Please choose the symbol corresponding to the math operation that you would like to complete
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+#Addition
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+#Subtraction
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+
+#Multiplication
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+#Division
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+else:
+    print('Please choose a valid math operation before continuing')
+
+
+
+input("Press [Enter] to exit the Calculator!")
 
