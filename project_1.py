133a739edc07a41fa62bda02d92e7c262fe2ad6b
belchejs-UC <97567968+belchejs-UC@users.noreply.github.com>
2022-02-20 21:24:09 -0500

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -1,35 +1,36 @@
 operation = input('''
 Please choose the symbol corresponding to the math operation that you would like to complete
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
+1 for addition
+2 for subtraction
+3 for multiplication
+4 for division
 ''')
 
-
+#
 number_1 = int(input('Enter your first number: '))
 number_2 = int(input('Enter your second number: '))
 
-#Addition
-if operation == '+':
+#Addition Operator 
+if operation == '1':
     print('{} + {} = '.format(number_1, number_2))
     print(number_1 + number_2)
 
-#Subtraction
-elif operation == '-':
+#Subtraction Operator
+elif operation == '2':
     print('{} - {} = '.format(number_1, number_2))
     print(number_1 - number_2)
 
-#Multiplication
-elif operation == '*':
+#Multiplication Operator
+elif operation == '3':
     print('{} * {} = '.format(number_1, number_2))
     print(number_1 * number_2)
 
-#Division
-elif operation == '/':
+#Division Operator
+elif operation == '4':
     print('{} / {} = '.format(number_1, number_2))
     print(number_1 / number_2)
 
+#If the user does not choose a valid operator, this message appears before the app closes  
 else:
     print('Please choose a valid math operation before continuing')
 
