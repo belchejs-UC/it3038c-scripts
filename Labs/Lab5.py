a1d3a57e50ba8f547a73005b0ac63e4054538f13
belchejs-UC <97567968+belchejs-UC@users.noreply.github.com>
2022-03-18 20:55:57 -0400

    Update Lab5.py
    Python code for converting someone's age to seconds

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,1 +1,10 @@
+#This line starts the program and has the user enter their age in years (eg. 22, 57,)
+Age = int(input('Please enter your age in years: '))
 
+#this line uses multiplication to convert days all the way to seconds
+seconds = Age * (365*24*60*60)
+
+#output of seconds
+print("Your age in seconds is: " +str(seconds))
+#output at end of program, prevents it from closing automatically
+input("Press [Enter] to exit the Years-to-Seconds Calculator!")
