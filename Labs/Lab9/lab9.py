367abb02fea669d53c0a217bc560ce7266b4e61f
belchejs-UC <97567968+belchejs-UC@users.noreply.github.com>
2022-03-27 22:26:08 -0400

    Update lab9.py

--- Labs/Lab9/lab9.py
+++ Labs/Lab9/lab9.py
@@ -1,11 +1,15 @@
 import json 
 import requests 
 
+#pulling information from localhost of widget.js
 r = requests.get('http://localhost:3000') 
+#noting that the data needed for the print statement is within the data presented on the server
 data=r.json() 
 i=0
+#list of colors needed in print statement
 color = ["blue", "green", "black"]
-#word =str(color[0])
+
+#while statement that grabs data of each widget in the list and displays it
 while i<len(data):
     print(data[i]['name'] + " is " + data[i]['color']) 
     i=i+1
