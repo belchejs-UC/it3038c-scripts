51b6e495243935a5250b3d19145f9774625274a4
belchejs-UC <97567968+belchejs-UC@users.noreply.github.com>
2022-04-17 17:23:11 -0400

    Update joecalculatorprojectv2.py

--- joecalculatorprojectv2.py
+++ joecalculatorprojectv2.py
@@ -1,8 +1,21 @@
 #I have created a function that runs the calculator called "calculation"
 def greetings():
-    print(''' Welcome to Joe's Calculator Program!
-          
-          ''')
+    print(''' 
+Welcome to Joe's Calculator Program!
+ _____________________
+|  _________________  |
+| | BY:JOE BELCHER  | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|      ''')
 
 #this message displays after the user chooses 'N' at the end screen
 def exitCalc():
@@ -17,29 +30,77 @@ Please choose the symbol corresponding to the math operation that you would like
 / for division
 ''')
 
-    number_1 = int(input('Enter your first number: '))
-    number_2 = int(input('Enter your second number: '))
+    number_1 = int(input('Enter your first integer: '))
+    number_2 = int(input('Enter your second integer: '))
 
 #Addition
     if operation == '+':
         print('{} + {} = '.format(number_1, number_2))
         print(number_1 + number_2)
+        result = int(number_1+number_2)
+        i=1
+        div=0
+        while i<=result:
+            if result%i==0:
+                div = div +1
+            i=i+1
+        if div == 2:
+            print('The answer is prime.')
+        else:
+            print('The answer is not prime.')
+
 
 #Subtraction
     elif operation == '-':
         print('{} - {} = '.format(number_1, number_2))
         print(number_1 - number_2)
-
+        result = int(number_1-number_2)
+        if result > 0:
+            i=1
+            div=0
+            while i<=result:
+                if result%i==0:
+                    div = div +1
+                i=i+1
+            if div == 2:
+                print('The answer is prime.')
+            else:
+                print('The answer is not prime.')
+        else:
+            print('The answer is not positive and therefore cannot be prime.')
 #Multiplication
     elif operation == '*':
         print('{} * {} = '.format(number_1, number_2))
         print(number_1 * number_2)
-
+        result = int(number_1*number_2)
+        i=1
+        div=0
+        while i<=result:
+            if result%i==0:
+                div = div +1
+            i=i+1
+        if div == 2:
+            print('The answer is prime.')
+        else:
+            print('The answer is not prime.')
 #Division
     elif operation == '/':
         print('{} / {} = '.format(number_1, number_2))
         print(number_1 / number_2)
-
+        result = int(number_1/number_2)
+        if result == number_1/number_2:
+            i=1
+            div=0
+            while i<=result:
+                if result%i==0:
+                    div = div +1
+                i=i+1
+            if div == 2:
+                print('The answer is prime.')
+            else:
+                print('The answer is not prime.')
+        else:
+            print('The answer is not an integer and cannot be prime')
     else:
         print('Please choose a valid math operation before continuing')
 
@@ -67,4 +128,3 @@ def repeat():
             
 greetings()
 calculation()
-
